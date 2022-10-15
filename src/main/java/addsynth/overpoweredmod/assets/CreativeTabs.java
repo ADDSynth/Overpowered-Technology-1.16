d33461bfce42b9ade3e802366bccd85da0d8068f
ADDSynth <addsynth@gmail.com>
2022-10-15 07:32:08 -0500

    Removed Tools Creative Tab

--- src/main/java/addsynth/overpoweredmod/assets/CreativeTabs.java
+++ src/main/java/addsynth/overpoweredmod/assets/CreativeTabs.java
@@ -1,12 +1,8 @@
 package addsynth.overpoweredmod.assets;
 
-import addsynth.overpoweredmod.config.Config;
-import addsynth.overpoweredmod.config.Features;
 import addsynth.overpoweredmod.game.reference.OverpoweredBlocks;
-import addsynth.overpoweredmod.game.reference.OverpoweredItems;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 
 public final class CreativeTabs {
 
@@ -17,30 +13,5 @@ public final class CreativeTabs {
       return new ItemStack(OverpoweredBlocks.inverter, 1);
     }
   };
-
-  /* DELETE
-  public static final ItemGroup machines_creative_tab =
-    Config.creative_tab_machines.get() ? new ItemGroup("overpowered_machines")
-    {
-      @Override
-      public final ItemStack createIcon(){
-        return new ItemStack(OverpoweredMod.registry.getItemBlock(Machines.generator), 1);
-      }
-    }
-  : creative_tab;
-  */
-
-  public static final ItemGroup tools_creative_tab =
-    Config.creative_tab_tools.get() ? new ItemGroup("overpowered_tools")
-    {
-      @Override
-      public final ItemStack makeIcon(){
-        return Features.celestial_tools.get() ? new ItemStack(OverpoweredItems.celestial_pickaxe, 1) :
-               Features.identifier.get()      ? new ItemStack(OverpoweredItems.unidentified_armor[2][0], 1) :
-               Features.void_tools.get()      ? new ItemStack(OverpoweredItems.void_sword, 1) :
-               new ItemStack(Items.STONE_SHOVEL, 1);
-      }
-    }
-  : creative_tab;
 
 }
